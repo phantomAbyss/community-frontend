aeb69b95a0f2caabbdac95c88426ff0dd0bd7c62
phantomAbyss <2873387076@qq.com>
2021-01-04 14:50:21 +0800

    init projects

---
+++ src/plugins/element.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input, Container, Header, Main, Aside, Menu, MenuItem, Submenu } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Submenu)
